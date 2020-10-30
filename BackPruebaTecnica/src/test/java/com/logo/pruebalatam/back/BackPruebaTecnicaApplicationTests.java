a902466625492f3d7747983143c3194d3bc640f2
Hernan Lopez <hlopezg@bch.bancodechie.cl>
2020-10-30 15:32:09 -0300

    generico back y front

---
+++ BackPruebaTecnica/src/test/java/com/logo/pruebalatam/back/BackPruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logo.pruebalatam.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
